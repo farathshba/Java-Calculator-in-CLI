ba07c8089f24232f313cb4e076b80800f92f9f9e
P1330787 <P1330787@NCS-130420KW01.ncs.corp.int-ads>
2021-01-03 00:32:09 +0800

    Initial Commit - Testing the Commit from Eclipse IDE

---
+++ src/com/example/functions/addition.java
@@ -0,0 +1,18 @@
+package com.example.functions;
+
+public class addition 
+{
+	private int i_num01;
+	private int i_num02;
+	
+	private double d_num01;
+	private double d_num02;
+	
+	private float f_num01;
+	private float f_num02;
+	
+	public addition()
+	{
+		
+	}
+}
---
+++ src/com/examples/trunk/mainClass.java
@@ -0,0 +1,9 @@
+package com.examples.trunk;
+
+public class mainClass 
+{
+	public static void main(String[] args)
+	{
+		
+	}
+}
